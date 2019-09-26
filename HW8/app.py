7b296594510372b215af26d8cc5498515d398f62
kjanghala <spo.kanika@gmail.com>
2019-09-25 21:14:25 -0700

    finished static routes

--- HW8/app.py
+++ HW8/app.py
@@ -35,42 +35,51 @@ def index():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/names<br/>"
-        f"/api/v1.0/passengers"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end<br/>"
     )
 
 
-@app.route("/api/v1.0/names")
-def names():
-    """Return a list of all passenger names"""
-    # Query all passengers
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    """Return a list of dates and respective precipitation levels in the last year of data"""
     session = Session(engine)
-    results = session.query(Passenger.name).all()
+    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date > '2016-08-22').all()
+    
+    date_list = []
+    prcp_list = []
+    for date, prcp in results:
+        date_list.append(date)
+        prcp_list.append(prcp)
 
-    # Convert list of tuples into normal list
+    date_prcp = dict(zip(date_list, prcp_list))
+    print(date_prcp)
+    
+    return jsonify(date_prcp)
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+    """Return a list of all station names"""
+    session = Session(engine)
+    results = session.query(Station.name).all()
+
     all_names = list(np.ravel(results))
 
     return jsonify(all_names)
 
-
-@app.route("/api/v1.0/passengers")
-def passengers():
-    """Return a list of passenger data including the name, age, and sex of each passenger"""
-    # Query all passengers
+@app.route("/api/v1.0/tobs")
+def tobs():
+    """Return a list of all observations"""
     session = Session(engine)
-    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
-
-    # Create a dictionary from the row data and append to a list of all_passengers
-    all_passengers = []
-    for name, age, sex in results:
-        passenger_dict = {}
-        passenger_dict["name"] = name
-        passenger_dict["age"] = age
-        passenger_dict["sex"] = sex
-        all_passengers.append(passenger_dict)
-
-    return jsonify(all_passengers)
-
+    results = session.query(Measurement.tobs).filter(Measurement.date > '2016-08-22').all()
+       
+    yr_tobs = list(np.ravel(results))
+        
+    return jsonify(yr_tobs)
 
 if __name__ == '__main__':
     app.run(debug=True)
