8c19989dec6d65d6eb742a64874fb88b7660641d
kjanghala <spo.kanika@gmail.com>
2019-09-25 21:57:21 -0700

    finished

--- HW8/app.py
+++ HW8/app.py
@@ -73,13 +73,55 @@ def stations():
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-    """Return a list of all observations"""
+    """Return a list of all observations of the most active station"""
     session = Session(engine)
-    results = session.query(Measurement.tobs).filter(Measurement.date > '2016-08-22').all()
+    results = session.query(Measurement.tobs).filter(Measurement.station == 'USC00519281').all()
        
     yr_tobs = list(np.ravel(results))
         
     return jsonify(yr_tobs)
+
+@app.route("/api/v1.0/<start>")
+def start_only(start):
+    """Return a calculation of min, max, and avg for all dates greater than and equal to the start date"""
+    start = str(start)
+    print(start)
+    session = Session(engine)
+    results = session.query(Measurement.tobs).filter(Measurement.date >= start).all()
+    
+    res_list = list(np.ravel(results))
+    print(res_list)
+    
+    low_temp = min(res_list)
+    high_temp = max(res_list)
+    avg_temp = sum(res_list)/len(res_list)
+        
+    return(
+        f"lowest temperature: {low_temp}<br/>"
+        f"highest temperature: {high_temp}<br/>"
+        f"average temperature: {avg_temp}"
+    )
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start, end):
+    """Return a calculation of min, max, and avg for all dates greater than and equal to the start date and less than or equal to the end date"""
+    start = str(start)
+    end = str(end)
+    session = Session(engine)
+    results = session.query(Measurement.tobs).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    
+    res_list = list(np.ravel(results))
+    print(res_list)
+    
+    low_temp = min(res_list)
+    high_temp = max(res_list)
+    avg_temp = sum(res_list)/len(res_list)
+        
+    return(
+        f"lowest temperature: {low_temp}<br/>"
+        f"highest temperature: {high_temp}<br/>"
+        f"average temperature: {avg_temp}"
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
